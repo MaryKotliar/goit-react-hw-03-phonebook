da93e42b2547c189f61c9cec9a6870e643202f96
MaryKotliar <glinskayam@ukr.net>
2022-12-14 12:43:52 +0200

    After mentor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,14 @@ import { GlobalStyle } from './GlobalStyle';
 
 export class App extends Component {
   static propTypes = {};
-  static defaultProps = {
-    initialContacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-  };
+
+  initialContacts = [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ];
+
   state = {
     contacts: [],
 
@@ -26,7 +26,7 @@ export class App extends Component {
     if (savedContacts !== null) {
       this.setState({ contacts: savedContacts });
     } else {
-      this.setState({ contacts: this.props.initialContacts });
+      this.setState({ contacts: initialContacts });
     }
   }
   componentDidUpdate(_, prevState) {
